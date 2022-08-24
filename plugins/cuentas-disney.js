4ab259d5c87478868118c87466f1c0de897707c2
NeKosmic-NK <104524366+NeKosmic-NK@users.noreply.github.com>
2022-08-24 11:26:23 +0100

    Create cuentas-disney.js

---
+++ plugins/cuentas-disney.js
@@ -0,0 +1,22 @@
+let handler  = async (m, { conn }) => {
+conn.reply(m.chat,`*┌────「 DISNEY+ 」─*\n*“${pickRandom(global.verdad)}”*\n*└────「 CUENTA GENERADA 」─*`, m)
+}
+handler.help = ['disney']
+handler.tags = ['fun']
+handler.command = /^disney/i
+export default handler
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
+
+global.disney = [
+  "Correo: rhcomets34@gmail.com Contraseña: Panthers23",
+  "Correo:jaylenwalker15@gmail.com Contraseña: Motorcycle12",
+  "Correo: smitetha987@gmail.com Contraseña: smith1234",
+  "Correo: morganpjason@gmail.com Contraseña: Jasonmorgan12",
+  "Correo: nilesbanks@gmail.com Contraseña: niles.25",
+  "Correo: julien.bestron@gmail.com Contraseña: Sucesmoi54",
+  "Correo: natalie@nativeno.com Contraseña: 4Me2know",
+
+] 
