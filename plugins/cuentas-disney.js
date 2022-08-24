5c5e43cabb27131fab3300e3a0e07d6fe44da086
NeKosmic-NK <104524366+NeKosmic-NK@users.noreply.github.com>
2022-08-24 11:27:08 +0100

    Update cuentas-disney.js

--- plugins/cuentas-disney.js
+++ plugins/cuentas-disney.js
@@ -1,5 +1,5 @@
 let handler  = async (m, { conn }) => {
-conn.reply(m.chat,`*┌────「 DISNEY+ 」─*\n*“${pickRandom(global.verdad)}”*\n*└────「 CUENTA GENERADA 」─*`, m)
+conn.reply(m.chat,`*┌────「 DISNEY+ 」─*\n*“${pickRandom(global.disney)}”*\n*└────「 CUENTA GENERADA 」─*`, m)
 }
 handler.help = ['disney']
 handler.tags = ['fun']
