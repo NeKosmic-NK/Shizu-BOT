4c79af381282a72bce4984c89ac8e601c29d3d4f
NeKosmic-NK <104524366+NeKosmic-NK@users.noreply.github.com>
2022-08-24 11:52:48 +0100

    Update menu-audios.js

--- plugins/menu-audios.js
+++ plugins/menu-audios.js
@@ -1,4 +1,4 @@
-﻿const { levelling } = '../lib/levelling.js'
+const { levelling } = '../lib/levelling.js'
 import PhoneNumber from 'awesome-phonenumber'
 let handler = async (m, { conn, usedPrefix }) => {
 let pp = './media/menu/Menuaudios.jpg'
@@ -6,7 +6,7 @@ try {
 } catch (e) {
 } finally {
 //let about = (await conn.getStatus(who).catch(console.error) || {}).status || ''
-let username = await conn.getName(m.sender)
+let name = await conn.getName(m.sender)
 let str = `
 *ミ💖 𝗛𝗢𝗟𝗔 _${username}_ 💖彡*
 𝗕𝗜𝗘𝗡𝗩𝗘𝗡𝗜𝗗𝗢 𝗔𝗟 𝗠𝗘𝗡𝗨 𝗔𝗨𝗗𝗜𝗢𝗦
@@ -82,9 +82,12 @@ let str = `
 ° 🔊 _tumadre_ (video)
 ° 🔊 _Sos puto_
 `.trim()
+conn.sendHydrated(m.chat, str, wm, pp, 'https://github.com/BrunoSobrino/TheMystic-Bot-MD', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
+['𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '/menu']
+], m)
+}}
 handler.help = ['menu2', 'help2', '?2', 'menuaudios']
 handler.tags = ['main']
-handler.command = /^(menuaudio|menuaudios|memuaudios|memuaudio|audios)$/i
+handler.command = /^(menu2|audios|menú2|memu2|menuaudio|menuaudios|memuaudios|memuaudio|audios|audio)$/i
 handler.fail = null
-handler.register = true
 export default  handler
